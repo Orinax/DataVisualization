192f7af491932e33f0c79d40ee268233c63ea6d5
Orinax <chaunceypelton@gmail.com>
2022-04-09 22:50:50 +0700

    Tried to use a broken mirror...

--- rw_visual.py
+++ rw_visual.py
@@ -12,6 +12,18 @@ while True:
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Greens,
                 edgecolor='none', s=15)
+
+# Something about this block doesn't work at all.
+# No points are visible if this block is included.
+#    # Remove the axes.
+#    plt.axes().get_xaxis().set_visible(False)
+#    plt.axes().get_yaxis().set_visible(False)
+
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c='blue', edgecolor='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+                s=100)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
